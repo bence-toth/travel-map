423296b5aef4947b49e01e50367bcbe67ef1dd2c
Bence A. Tóth <tothab@gmail.com>
2022-11-11 12:03:15 +0100

    Added travel to-dos

--- src/travels.ts
+++ src/travels.ts
@@ -2,6 +2,38 @@ export interface Travel {
   countryCode: string;
 }
 
+// TODO:
+// Copenhagen
+// Dragør
+// Roskilde
+// Malmö
+// Mölle
+// Helsingborg
+// Budapest
+// Pécs
+// Budakeszi
+// Orfu
+// Abaliget
+// Veresegyhaz
+// Hurghada
+// Cairo
+// Alexandria
+// Luxor
+// Siwa
+// Aswan
+// Abu Simbel
+// Gdansk
+// Skoplje
+// Prstina
+// Kotor
+// Sarajevo
+// Mostar
+// Caplijna
+// Ploce
+// Korcula
+// Lumbarda
+// Dijarbakir
+
 const travels = [
   {
     countryCode: "ZM",

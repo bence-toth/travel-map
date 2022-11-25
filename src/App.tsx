07b8ac9a69256c120d399e0c2be1d11a46e14112
Bence A. Tóth <tothab@gmail.com>
2022-11-25 11:30:09 +0100

    Naive implementation of date slider

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
+import { useState, useCallback } from "react";
+import type { ChangeEvent } from "react";
+
 import Map from "./Map";
 
 import travels from "./travels";
 
-const App = () => <Map travels={travels} />;
+const now = Date.now();
+
+const App = () => {
+  const [date, setDate] = useState(travels[0].date.getTime());
+
+  const updateDate = useCallback((event: ChangeEvent<HTMLInputElement>) => {
+    setDate(Number(event.target.value));
+  }, []);
+
+  return (
+    <>
+      <Map travels={travels} />
+      <input
+        type="range"
+        min={travels[0].date.getTime()}
+        max={now}
+        value={date}
+        onChange={updateDate}
+        step={24 * 60 * 60 * 1000}
+      />
+      <span style={{ color: "white" }}>{date}</span>
+    </>
+  );
+};
 
 export default App;

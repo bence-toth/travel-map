e2a4fef9a94d6ba176052a543c3839466debfbf8
Bence A. Tóth <tothab@gmail.com>
2022-11-25 16:35:05 +0100

    Handling window wheel events

--- src/useDate.ts
+++ src/useDate.ts
@@ -1,7 +1,11 @@
-import { useState, useCallback, useMemo } from "react";
+import { useState, useCallback, useMemo, useEffect } from "react";
 import type { ChangeEvent } from "react";
 
 import travels from "./travels";
+
+const oneDay = 24 * 60 * 60 * 1000;
+
+const now = Date.now();
 
 const useDate = () => {
   const [date, setDate] = useState(travels[0].date.getTime());
@@ -14,6 +18,24 @@ const useDate = () => {
   );
 
   const firstTravelDate = useMemo(() => travels[0].date.getTime(), []);
+
+  useEffect(() => {
+    const onHandleWindowWheel = (event: WheelEvent) => {
+      if (event.deltaY < 0 && date < now) {
+        setDate((previousDate) => Math.min(previousDate + oneDay, now));
+      } else if (event.deltaY > 0 && date > firstTravelDate) {
+        setDate((previousDate) =>
+          Math.max(previousDate - oneDay, firstTravelDate)
+        );
+      }
+    };
+
+    window.addEventListener("wheel", onHandleWindowWheel);
+
+    return () => {
+      window.removeEventListener("wheel", onHandleWindowWheel);
+    };
+  }, [date, firstTravelDate]);
 
   return { date, onHandleDateChange, firstTravelDate };
 };

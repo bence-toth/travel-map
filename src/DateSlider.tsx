a8f201bdb50ee6077dade5fa091ed4c5906db7eb
Bence A. Tóth <tothab@gmail.com>
2022-11-25 11:52:50 +0100

    Formatted date display

--- src/DateSlider.tsx
+++ src/DateSlider.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import type { ChangeEvent } from "react";
 
 import "./DateSlider.css";
@@ -10,23 +11,34 @@ interface DateSliderProps {
 
 const now = Date.now();
 
+const dateFormatter = new Intl.DateTimeFormat("en-GB", {
+  dateStyle: "long",
+});
+
 const DateSlider = ({
   date,
   onHandleDateChange,
   firstTravelDate,
-}: DateSliderProps) => (
-  <div className="date-slider-wrapper">
-    <input
-      className="date-slider"
-      type="range"
-      min={firstTravelDate}
-      max={now}
-      value={date}
-      onChange={onHandleDateChange}
-      step={24 * 60 * 60 * 1000}
-    />
-    <span className="date-display">{date}</span>
-  </div>
-);
+}: DateSliderProps) => {
+  const formattedDate = useMemo(() => dateFormatter.format(date), [date]);
+  return (
+    <>
+      <div className="date-slider-wrapper">
+        <input
+          className="date-slider"
+          type="range"
+          min={firstTravelDate}
+          max={now}
+          value={date}
+          onChange={onHandleDateChange}
+          step={24 * 60 * 60 * 1000}
+        />
+      </div>
+      <div className="date-display-wrapper">
+        <span className="date-display">{formattedDate}</span>
+      </div>
+    </>
+  );
+};
 
 export default DateSlider;
